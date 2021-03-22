8632c4a39f31797209f411245a97533b048e87ae
Miguel florian <migflor04@gmail.com>
2021-03-22 22:51:09 +0100

    Update app view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to='/'>Accueil</router-link> | 
-    <router-link to='/register'>S'inscrire</router-link>
+    <router-link to='/register'>Pas de compte? S'inscrire</router-link>
   </nav>
   <router-view> </router-view>
 </template>
@@ -22,4 +22,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+a{
+    font-weight: bold;
+    text-decoration: none;
+     color: #2c3e50b6;
+}
+.router-link-active{
+  font-weight: bold;
+    text-decoration: none;
+     color: #EF5A31;
+     border-bottom: 2px solid #EF5A31;
+  }
 </style>
