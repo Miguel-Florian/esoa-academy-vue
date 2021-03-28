1f7777539e903bf3598d917c6121ba18ef7043f0
Miguel florian <migflor04@gmail.com>
2021-03-28 14:35:53 +0100

    add url link to helvetica font

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,9 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Helvetica&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
