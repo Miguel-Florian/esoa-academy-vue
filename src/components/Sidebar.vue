1f062fc31d0258d34d87f46d71bfb5e6aec1067b
Miguel florian <migflor04@gmail.com>
2021-03-28 14:37:10 +0100

    add a sidebar of dashboard

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,25 +1,29 @@
 <template>
     <div class="sidebar">
-        <div class="title">Simple sidebar</div>
+        <div class="title">Dashboard</div>
         <div class="menu-items">
-            <router-link to="/overview" active-class="active" tag="button" class="side-btn">
-                <div class="link-container">Overview</div>                    
+            <router-link to="/dashboard/overview" active-class="active" exact tag="button" class="side-btn">
+                <div class="link-container">Ajouter un académicien</div>                    
             </router-link>
-            <router-link to="/accueil" active-class="active" tag="button" class="side-btn">
-                <div class="link-container">Accueil</div>                    
+            <router-link to="/dashboard/accueil" active-class="active" exact tag="button" class="side-btn">
+                <div class="link-container">Modifier un académicien</div>                    
             </router-link>
-            <router-link to="/messages" active-class="active" tag="button" class="side-btn">
-                <div class="link-container">Message</div>                    
+            <router-link to="/dashboard/messages" active-class="active" exact tag="button" class="side-btn">
+                <div class="link-container">Supprimer un académicien</div>                    
             </router-link>
-            <router-link to="/profil" active-class="active" tag="button" class="side-btn">
-                <div class="link-container">Profil</div>                    
+            <router-link to="/dashboard/rencontres" active-class="active" exact tag="button" class="side-btn">
+                <div class="link-container">Consulter les rencontres</div>                    
+            </router-link><router-link to="/dashboard/finances" active-class="active" exact tag="button" class="side-btn">
+                <div class="link-container">Consulter les finances</div>                    
             </router-link>
-            <router-link to="/connexion" active-class="active" tag="button" class="side-btn">
-                <div class="link-container">Connexion</div>                    
+            <router-link to="/dashboard/profil" active-class="active" exact tag="button" class="side-btn">
+                <div class="link-container">Afficher profil</div>                    
+            </router-link>
+            <router-link to="/dashboard/mail" active-class="active" exact tag="button" class="side-btn">
+                <div class="link-container">Envoie d'email</div>                    
             </router-link>
             
         </div>
-        
     </div>
 </template>
 
@@ -31,36 +35,80 @@
 
 <style scoped>
 .title{
-    color:white;
+    color:rgb(143, 136, 136);
     font-size:24px;
-    margin-top:10px;
+    margin-top:10%;
+    font-weight: bold;
+    font-family: Helvetica;
 }
 .menu-items{
     display:flex;
     flex-direction:column;
-    margin-top:40px;
+    margin-top:1%;
 }
 .menu-items>*{
-    margin-top:22%;
+    margin-top:16%;
 }
 .side-btn:focus{
     outline:none;
 }
+.side-btn.active::before{
+    position:absolute;
+    content:"";
+    top:-18px;
+    right: 0;
+    height: 18px;
+    width: 18px;
+    background-color: white;
+}
+.side-btn.active::after{
+    position:absolute;
+    content:"";
+    bottom:-18px;
+    right: 0;
+    height: 18px;
+    width: 18px;
+    background-color: white;
+}
 .side-btn.active{
+    position: relative;
     background-color: #fff;
-    color:teal;
+    color:#e0291c;
     font-weight: bold;
+    border-radius:30px 0 0 30px;
+    margin-left:10px;
 }
 .side-btn{
     font-size:18px;
     border:none;
     padding:10px 0px;
     cursor: pointer;
-    color: black;
+    color:white;
     text-decoration: none;
     width: auto;
     height: auto;
-    background-color: #fff;
+    background-color: transparent;
+}
+.side-btn.active .link-container::before{
+    position:absolute;
+    top:-30px;
+    right:0;
+    content:"";
+    height: 30px;
+    width: 30px;
+    border-radius: 50%;
+    background-color:#233E6A;
+}
+.side-btn.active .link-container::after{
+    position:absolute;
+    bottom:-30px;
+    right:0;
+    content:"";
+    height: 30px;
+    width: 30px;
+    border-radius: 50%;
+    background-color:#233E6A;
+    z-index:99;
 }
 
 </style>
