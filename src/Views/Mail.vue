280a21ea2598b2dfdb75edd4214f78efcc65252f
Miguel florian <migflor04@gmail.com>
2021-03-28 14:39:56 +0100

    Mail view

---
+++ src/Views/Mail.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="overview">
+        <p>Mail</p>
+   </div>
+</template>
+<script>
+export default {
+    name:"Mail"
+    
+}
+</script>
+
+<style scoped>
+    .overview{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        font-size:30px;
+        color:gray;
+        font-weight: 700;
+    }
+</style>
