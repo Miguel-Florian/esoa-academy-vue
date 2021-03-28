5d1aa189eb5084c9f551af6bf532d8c6d1511e66
Miguel florian <migflor04@gmail.com>
2021-03-28 14:40:33 +0100

    update dashboard view

--- src/Views/Dashboard.vue
+++ src/Views/Dashboard.vue
@@ -2,8 +2,12 @@
 <div class="dashboard">
     <Sidebar/>
     <div class="content">
+        <div class="user">
+                <h5>Daniel Ngantchou &nbsp; <i class="fas fa-user"></i></h5>
+        </div>
         <router-view/>
     </div>
+    
 </div>
 </template>
 
@@ -24,20 +28,28 @@
      box-sizing:border-box;
      
  }
- template{
-    background-color: teal;
-  }
  .dashboard{
      display:grid;
      grid-template-columns: 1fr 5fr;
      height: 100vh;
      width: 100vw;
-     background-color:teal;
+     background-color:#233E6A;
+     margin-left:-1%;
+     margin-top:-1%;
+     margin-bottom:-1%;
+ }
+ .user{
+     margin-left:80%;
+     justify-content: center;
+     align-items: center;
+ }
+ i{
+     color:#EF5A31;
  }
  .content{
     background-color: white;
     border-radius: 10px;
-    margin-top:2%;
+    margin-top:4%;
     margin-right:2%;
     margin-bottom: 2%;
  }
