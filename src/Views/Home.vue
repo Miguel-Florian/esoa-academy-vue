d4f58e3b05c3b8be1da4d7e96d8b0c04b62700ec
“Miguel-Florian” <migflor04@gmail.com>
2021-07-17 15:18:38 +0100

    update UX of login vue

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -1,86 +1,92 @@
 <template>
-<nav>
-    <router-link to='/'>Accueil</router-link> | 
-    <router-link to='/register'>Pas de compte? S'inscrire</router-link>
+  <nav>
+      <router-link to='/'>Accueil</router-link> | 
+      <router-link to='/register'>Pas de compte? S'inscrire</router-link>
   </nav>
-  <div id="login">
-       <div class="logo"><img src="../assets/esoa-academy.png" alt=""></div>
-    <h1>Login</h1>
-    <div class="picture"><img src="../assets/cherry-125.png" alt="" class="img-icon"></div>
-    <form>
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input
-          type="text"
-          class="username"
-          id="username"
-          placeholder="Enter username"
-        />
-      </div><br>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input
-          type="password"
-          class="password"
-          id="password"
-          placeholder="Password"
-        />
-      </div><br>
-      <div class="form-check">
-        <input type="checkbox" class="checkbox" id="checkbox" />
-        <label class="form-check-label" for="checkbox">Remember me </label>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+  <div class="container">
+    <div class="logo"><img src="../assets/esoa-academy.png" alt=""></div>
+    <div class="content-form">
+      <div class="title"><h2>Connexion</h2></div>
+
+      <form action="">
+        <div class="input-title-group">
+          <div class="input-title">Nom d'utilisateur :</div>
+          <input type="text" class="username" id="username" placeholder="Enter username"/>
+        </div>
+
+        <div class="input-title-group">
+          <div class="input-title">Mot de passe :</div>
+          <input type="password" class="password" id="password" placeholder="Enter password"/>
+        </div>
+
+        <div class="input-check">
+          <input type="checkbox" class="checkbox" id="checkbox" />
+          <label class="form-check-label" for="checkbox">Remember me </label>
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+
+    </div>
     <div class="footer">
       <h4>Copyright &copy; Esoa Academy 2021 - Tous droits réservés &reg;</h4>
     </div>
   </div>
+  
 </template>
 <style scoped>
+nav{
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
 a{
     font-weight: bold;
     text-decoration: none;
-     color: #2c3e50b6;
+    color: #2c3e50b6;
 }
 .router-link-active{
   font-weight: bold;
-    text-decoration: none;
-     color: #EF5A31;
-     border-bottom: 2px solid #EF5A31;
-  }
-#login{
-    margin-top:2%;
-    border:0px solid black;
-    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.7);
-    border-radius: 60px;
+  text-decoration: none;
+  color: #EF5A31;
+  border-bottom: 2px solid #EF5A31;
 }
 .logo{
-    width: 180px;
-    height: 200px;
-    margin: 0 auto;
+  margin-top: 2em;
 }
-.picture{
-    float:left;
-    margin-left:20%;
+.content-form{
+  border: 0px solid black;
+  padding: 2em;
+  width: 30%;
+  height: 50vh;
+  margin: 0 auto;
+  background-image: url(../assets/tdf5.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 20px;
 }
-.img-icon{
-    width: 280px;
-    height: 400px;
+.title h2{
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
 }
 form{
-    float:right;
-    margin-right:26%;
+  display: flex;
+  flex-direction: column;
+  gap: 1.2em;
 }
-label{
-    margin-right:1em;
+.input-title-group{
+  display: flex;
+  flex-direction: column;
 }
-label,button{
-    font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans -serif;
-    font-weight: bold;
+.input-title-group .input-title{
+  display: flex;
+  flex-flow: row;
+  margin-bottom: 1em;
+  color: #fff;
+  font-size: 18px;
 }
 input[type="text"],input[type="password"]{
-    width: 20em;
+    width: 95%;
     height: 2em;
     padding-left:1em;
     outline: none;
@@ -99,12 +105,15 @@ input[type="text"]:hover,input[type="password"]:hover{
   font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans -serif;
   font-weight: 400px; 
   }
-.form-group,.form-check,button{
-    margin-top:16%;
-}
+.input-check .form-check-label{
+    color:#EF5A31;
+    font-size: 18px;
+    font-weight: 700;
+    text-decoration: underline;
+  }
 button{
-    width: 20em;
-    height: 3em;
+    width: 100%;
+    height: 2em;
     background-color: #213F6B;
     border-radius: 20px;
     color: #EF5A31;
@@ -119,11 +128,102 @@ button:hover{
   background-color: #e0291c;
   color: #FFFFFF;
 }
-.footer{
-  margin-top:40%;
-  position: relative;
+/*----------Responsive for max-width: 320px, width 320px-375px ------------*/
+@media   screen and (max-width: 320px), (max-width:375px) and (min-width: 321px) {
+  nav{
+    margin-top:2em;
+  }
+  .logo{
+    width: 40vw;
+    height: 22vh;
+    margin: 0 auto;
+    margin-bottom: 1em;
+  }
+  .logo img{
+    width: 100%;
+    height: 100%;
+  }
+  .content-form{
+    width: 80%;
+    padding: 1em;
+    height: 60vh;
+    background-image: url(../assets/tdf4.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+  } 
+  .input-check .form-check-label {
+    font-size: 16px;
+    color: #fff;
+  }
 }
-h4{
-  text-decoration: underline;
+/*-------------Responsive for width 320px-375px -------*/
+@media screen and (max-width:375px) and (min-width: 321px){
+  .logo{
+    width: 30vw;
+    height: 22vh;
+    margin: 0 auto;
+    margin-bottom: 1em;
+  }
+  .logo img{
+    width: 100%;
+    height: 100%;
+  }
+}
+/*-----------Responsive for width 375px-425px --------------*/
+@media screen and (max-width:425px) and (min-width: 376px){
+  nav{
+    margin-top:2em;
+  }
+  .logo{
+    width: 38vw;
+    height: 28vh;
+    margin: 0 auto;
+    margin-bottom: 1em;
+  }
+  .logo img{
+    width: 100%;
+    height: 100%;
+  }
+  .content-form{
+    width: 80%;
+    padding: 1em;
+    height: 60vh;
+    background-image: url(../assets/tdf4.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+  } 
+  .input-check .form-check-label {
+    font-size: 16px;
+    color: #fff;
+  }
+
+}
+/** Responsive de 425px à 768px */
+@media screen and (max-width:768px) and (min-width: 426px) {
+  nav{
+    margin-top:2em;
+  }
+  .logo{
+    width: 26vw;
+    height: 22vh;
+    margin: 1em auto;
+    margin-bottom: 1em;
+  }
+  .logo img{
+    width: 100%;
+    height: 100%;
+  }
+  .content-form{
+    width: 80%;
+    padding: 1em;
+    height: 60vh;
+    background-image: url(../assets/tdf3.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+  } 
+  .input-check .form-check-label {
+    font-size: 16px;
+    color: #fff;
+  }
 }
 </style>
