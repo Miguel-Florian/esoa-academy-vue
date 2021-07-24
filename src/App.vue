1b485940cbe2fdd3c6be97923af32ab6fedbed44
“Miguel-Florian” <migflor04@gmail.com>
2021-07-24 11:01:29 +0100

    updating ...

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,5 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-.router-link-active{
-  font-weight: bold;
-    text-decoration: none;
-     color: #EF5A31;
-     border-bottom: 2px solid #EF5A31;
-  }
+
 </style>
